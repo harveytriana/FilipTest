7f37decc1b7c3e090918f71460a52f3e076a5802
Harvey Triana <harveytriana@hotmail.com>
2018-10-17 10:53:48 -0500

    Initial commit

---
+++ FilipTest/Controllers/BooksController.cs
@@ -0,0 +1,56 @@
+﻿using MessagePack;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilipTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        // GET: api/Books
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return Book.Data;
+        }
+
+        // POST: api/Books
+        [HttpPost]
+        public void Post([FromBody] Book book)
+        {
+            Book.Data.Add(new Book {
+                Author = book.Author,
+                Title = book.Title
+            });
+        }
+    }
+
+    [MessagePackObject]
+    public class Book
+    {
+        public static List<Book> Data = new List<Book>
+            {
+                new Book {
+                    Title = "Our Mathematical Universe: My Quest for the Ultimate Nature of Reality",
+                    Author = "Max Tegmark",
+                    Price = 935.23,
+                    Words = 142524},
+                new Book {
+                    Title = "Hockey Towns",
+                    Author = "Ron MacLean",
+                    Price = 1235.23,
+                    Words = 654456},
+            };
+
+        [Key(0)]
+        public string Title { get; set; }
+        [Key(1)]
+        public string Author { get; set; }
+        [Key(2)]
+        public double Price { get; set; }
+        [Key(3)]
+        public long Words { get; set; }
+
+    }
+}
---
+++ FilpTestConsole/Program.cs
@@ -0,0 +1,124 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+// WebApiContrib.Core.MessagePack.Tests
+
+namespace FilpTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FILIP TEST");
+
+            Console.WriteLine("\nPause");
+            Console.ReadKey();
+
+            //DoGetJson();
+
+            PostMessagePack();
+            DoGetMessagePack();
+
+            Console.WriteLine("\nPause");
+            Console.ReadKey();
+        }
+
+        private static void DoGetJson()
+        {
+            var apiRoot = "http://localhost:52647";
+
+            var httpClient = new HttpClient() {
+                BaseAddress = new Uri(apiRoot)
+            };
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var rawData = httpClient.GetStringAsync("/api/books/").Result;
+
+            var books = JsonConvert.DeserializeObject<List<Book>>(rawData);
+
+            books.ForEach(x => {
+                Console.WriteLine(x);
+            });
+
+            httpClient.Dispose();
+        }
+
+        private static void DoGetMessagePack()
+        {
+            var apiRoot = "http://localhost:52647";
+
+            var httpClient = new HttpClient() {
+                BaseAddress = new Uri(apiRoot)
+            };
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-msgpack"));
+
+            var rawData = httpClient.GetStreamAsync("/api/books/").Result;
+
+            var books = MessagePackSerializer.Deserialize<List<Book>>(rawData);
+
+            books.ForEach(x => {
+                Console.WriteLine(x);
+            });
+
+            httpClient.Dispose();
+        }
+
+        private static void PostMessagePack()
+        {
+            var apiRoot = "http://localhost:52647";
+
+            var httpClient = new HttpClient() {
+                BaseAddress = new Uri(apiRoot)
+            };
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+
+            var book = new Book {
+                Author = "Harvey Triana",
+                Title = "The improvements over SignalR",
+                Price = 6542.25,
+                Words = 987645
+            };
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-msgpack"));
+
+            var content = new ByteArrayContent(MessagePackSerializer.Serialize(book, ContractlessStandardResolver.Instance));
+
+            var response = httpClient.PostAsync("/api/books/", content).Result;
+
+            if (response.IsSuccessStatusCode) {
+                Console.WriteLine("Success.");
+            } else {
+                Console.Write("Error");
+            }
+
+            httpClient.Dispose();
+        }
+    }
+
+
+
+
+    [MessagePackObject]
+    public class Book
+    {
+        [Key(0)]
+        public string Title { get; set; }
+        [Key(1)]
+        public string Author { get; set; }
+        [Key(2)]
+        public double Price { get; set; }
+        [Key(3)]
+        public long Words { get; set; }
+
+        public override string ToString()
+        {
+            return $"Author: {Author} Title: {Title} Price: {Price} Words: {Words}";
+        }
+    }
+}
